114923fb2c393bfe35a1fd12859bf49411ec5e5e
yanghyeonjin <guswls7081@gmail.com>
2020-07-14 16:26:22 +0900

    * useState를 통해 컴포넌트에서 바뀌는 값 관리하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hello from './components/Hello';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
       <Hello />
+      <Counter />
     </div>
   );
 }
