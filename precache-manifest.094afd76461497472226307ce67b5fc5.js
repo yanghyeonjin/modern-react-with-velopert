f2b926db2d1c62e0a340625a68b2f82b089a25ad
yanghyeonjin <guswls7081@gmail.com>
2020-07-14 16:07:10 +0900

    Updates

---
+++ precache-manifest.094afd76461497472226307ce67b5fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d538639897728b5ab940d3edad5c9a0f",
+    "url": "/modern-react-with-velopert/index.html"
+  },
+  {
+    "revision": "6c364f2dddaf3509c397",
+    "url": "/modern-react-with-velopert/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b290a3655c8df396aea",
+    "url": "/modern-react-with-velopert/static/js/2.2c9939a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/modern-react-with-velopert/static/js/2.2c9939a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c364f2dddaf3509c397",
+    "url": "/modern-react-with-velopert/static/js/main.aa6b0a69.chunk.js"
+  },
+  {
+    "revision": "90f16c33a6660891b19c",
+    "url": "/modern-react-with-velopert/static/js/runtime-main.e00b80b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/modern-react-with-velopert/static/media/logo.5d5d9eef.svg"
+  }
+]);
