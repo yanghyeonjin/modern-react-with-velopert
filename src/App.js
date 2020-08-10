3e66d26f003e0cb06db7e877b79a9357df6f619c
yanghyeonjin <guswls7081@gmail.com>
2020-08-10 13:07:31 +0900

    * button 스타일링

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React, { useMemo, useReducer, createContext } from 'react';
+import React, { useMemo, useReducer, createContext } from "react";
 
-import User from './components/User';
-import CreateUser from './components/CreateUser';
-
+import User from "./components/User";
+import CreateUser from "./components/CreateUser";
+import Button from "./components/Button";
+import "./App.css";
 
 const initialState = {
   users: [
     {
       id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: true
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]
-}
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ],
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'CREATE_USER':
+    case "CREATE_USER":
       return {
         ...state,
         users: [
@@ -37,36 +38,34 @@ const reducer = (state, action) => {
           {
             id: action.id,
             username: action.username,
-            email: action.email
-          }
-        ]
-      }
-    case 'TOGGLE_USER':
+            email: action.email,
+          },
+        ],
+      };
+    case "TOGGLE_USER":
       return {
         ...state,
-        users: state.users.map(user => user.id === action.id ? { ...user, active: !user.active } : user)
-      }
-    case 'REMOVE_USER':
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
       return {
         ...state,
-        users: state.users.filter((user) => user.id !== action.id)
-      }
+        users: state.users.filter((user) => user.id !== action.id),
+      };
     default:
       return state;
   }
-}
-
+};
 
 function countActiveUsers(users) {
-  console.log('활성 사용자 수 세는 중...');
+  console.log("활성 사용자 수 세는 중...");
   return users.filter((user) => user.active).length;
 }
 
-
 // Context API 사용
 export const UserDispatch = createContext(null);
-
-
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -74,13 +73,48 @@ function App() {
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
-
   return (
-    <UserDispatch.Provider value={dispatch}>
-      <CreateUser />
-      {users.map(user => <User key={user.id} user={user} />)}
-      <div>활성사용자 수: {count}</div>
-    </UserDispatch.Provider>
+    // <UserDispatch.Provider value={dispatch}>
+    //   <CreateUser />
+    //   {users.map(user => <User key={user.id} user={user} />)}
+    //   <div>활성사용자 수: {count}</div>
+    // </UserDispatch.Provider>
+    <div className="App">
+      <div className="buttons">
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+    </div>
   );
 }
 
