c8425a4b10399eaf17c66d242d6dfcaad98dafdf
yanghyeonjin <guswls7081@gmail.com>
2020-08-21 12:00:09 +0900

    * Context API 를 사용한 전역 값 관리

--- begin-react/src/CreateUser.js
+++ begin-react/src/CreateUser.js
@@ -1,9 +1,35 @@
-import React, { useEffect } from 'react';
+// module
+import React, { useRef, useContext, useCallback } from 'react';
 
-function CreateUser({ username, email, onChange, onCreate }) {
-    useEffect(() => {
-        console.log('CreateUser render');
+// custom hook
+import useInputs from './hooks/useInputs';
+
+// context api
+import { UserDispatch } from './App';
+
+function CreateUser() {
+    const [{ username, email }, onChange, onReset] = useInputs({
+        username: '',
+        email: '',
     });
+
+    const nextId = useRef(4);
+
+    const dispatch = useContext(UserDispatch);
+
+    const onCreate = useCallback(() => {
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email,
+            },
+        });
+        onReset();
+        nextId.current += 1;
+    }, [username, email, onReset, dispatch]);
+
     return (
         <div>
             <input name="username" placeholder="계정명" onChange={onChange} value={username} />
