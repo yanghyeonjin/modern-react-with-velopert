99c69befddbd99b6948458f103c7c573736452c3
yanghyeonjin <guswls7081@gmail.com>
2020-08-21 09:45:51 +0900

    * JSX

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Hello from './Hello';
 
+// css
+import './App.css';
+
 function App() {
+    const name = 'react';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24, // 기본 단위 px
+        padding: '1rem', // 다른 단위 사용 시 문자열로 설정
+    };
     return (
-        <div>
-            <Hello />
-            <Hello />
-            <Hello />
-        </div>
+        <>
+            {/* 주석은 화면에 보이지 않습니다 */}
+            /* 중괄호로 감싸지 않으면 화면에 보입니다 */
+            <Hello
+            // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
+            />
+            <div style={style}>{name}</div>
+            <div className="gray-box"></div>
+        </>
     );
 }
 
