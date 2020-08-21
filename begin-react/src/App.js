0c5ccc3a71f6c162a01b10390d56ecb04673ef1c
yanghyeonjin <guswls7081@gmail.com>
2020-08-21 09:49:28 +0900

    * props 를 통해 컴포넌트에게 값 전달하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,27 +1,19 @@
+// module
 import React from 'react';
+
+// component
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 // css
 import './App.css';
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24, // 기본 단위 px
-        padding: '1rem', // 다른 단위 사용 시 문자열로 설정
-    };
     return (
-        <>
-            {/* 주석은 화면에 보이지 않습니다 */}
-            /* 중괄호로 감싸지 않으면 화면에 보입니다 */
-            <Hello
-            // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
-            />
-            <div style={style}>{name}</div>
-            <div className="gray-box"></div>
-        </>
+        <Wrapper>
+            <Hello name="react" color="red" />
+            <Hello color="pink" />
+        </Wrapper>
     );
 }
 
