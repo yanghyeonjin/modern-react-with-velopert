59e8dd705d01a9e279c7869a14f512724a89f4bf
yanghyeonjin <guswls7081@gmail.com>
2020-08-21 10:39:05 +0900

    * 배열에 항목 수정하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -27,16 +27,19 @@ function App() {
             id: 1,
             username: 'velopert',
             email: 'public.velopert@gmail.com',
+            active: true,
         },
         {
             id: 2,
             username: 'tester',
             email: 'tester@example.com',
+            active: false,
         },
         {
             id: 3,
             username: 'liz',
             email: 'liz@example.com',
+            active: false,
         },
     ]);
 
@@ -62,10 +65,14 @@ function App() {
         setUsers(users.filter((user) => user.id !== id));
     };
 
+    const onToggle = (id) => {
+        setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
+    };
+
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onRemove={onRemove} />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
         </>
     );
 }
