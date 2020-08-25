52dfd393192d337cbe25b9cc398d46893f1e3af8
yanghyeonjin <guswls7081@gmail.com>
2020-08-25 15:02:58 +0900

    * Link: 누르면 다른 주소로 이동시키기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 
@@ -10,6 +10,16 @@ import About from './About';
 function App() {
     return (
         <div>
+            {/* 리액트에서 a 태그를 사용하면 페이지를 아예 새로 불러오게 되어서 가지고 있던 state가 reset 된다. a 대신에 Link를 사용하자 */}
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+            </ul>
+
             <Route path="/" component={Home} exact={true} />
             <Route path="/about" component={About} />
         </div>
