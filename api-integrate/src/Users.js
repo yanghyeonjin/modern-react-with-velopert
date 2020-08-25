4a91a1c7cc4b4b4512353ac08ebd07c9fe4bbdd6
yanghyeonjin <guswls7081@gmail.com>
2020-08-25 11:06:30 +0900

    * useReducer로 요청 상태 관리하기

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null,
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null,
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error,
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
 function Users() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(reducer, { loading: false, data: null, error: null });
 
     // user 목록 불러오는 API
     const fetchUsers = async () => {
+        // 요청이 시작할 때에는 error와 users를 초기화하고 loading 상태를 true로 바꿉니다.
+        dispatch({ type: 'LOADING' });
         try {
-            // 요청이 시작할 때에는 error와 users를 초기화하고
-            setError(null);
-            setUsers(null);
-            setLoading(true); // loading 상태를 true로 바꿉니다.
-
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-            setUsers(response.data); // 데이터는 response.data안에 들어있습니다.
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            dispatch({ type: 'SUCCESS', data: response.data }); // 데이터는 response.data안에 들어있습니다.
         } catch (e) {
-            setError(e);
+            dispatch({ type: 'ERROR', error: e });
         }
-
-        setLoading(false);
     };
 
     useEffect(() => {
         fetchUsers();
     }, []);
+
+    const { loading, data: users, error } = state; // state.data를 users 키워드로 조회
 
     // 현재 상태에 따라 화면에 다르게 출력
     if (loading) {
