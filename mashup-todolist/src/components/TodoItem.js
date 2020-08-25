a9f3c6eca888a9db0be6008ffab3205f4db55c56
yanghyeonjin <guswls7081@gmail.com>
2020-08-25 10:22:51 +0900

    * .gitignore 추가 - .env.development * TodoItem 구현

--- mashup-todolist/src/components/TodoItem.js
+++ mashup-todolist/src/components/TodoItem.js
@@ -1,11 +1,14 @@
 // module
 import React, { useState } from 'react';
 import { MdDone, MdDelete } from 'react-icons/md';
+import { useTodoDispatch } from '../TodoContext';
+
 // css
 import './TodoItem.css';
 
 function TodoItem({ id, done, text }) {
     const [removeActive, setRemoveActive] = useState(false);
+    const dispatch = useTodoDispatch();
 
     // 마우스 hover 시, 삭제 버튼 다루기
     const showRemove = () => {
@@ -15,15 +18,27 @@ function TodoItem({ id, done, text }) {
         setRemoveActive(false);
     };
 
+    // todo 토클
+    const onToggle = () => {
+        dispatch({ type: 'TOGGLE', id });
+    };
+
+    // todo 삭제
+    const onRemove = () => {
+        dispatch({ type: 'REMOVE', id });
+    };
+
     return (
         <div className="todo-item" onMouseEnter={showRemove} onMouseLeave={hideRemove}>
-            <div className={`check-circle ${done && 'done'}`}>{done && <MdDone />}</div>
+            <div className={`check-circle ${done && 'done'}`} onClick={onToggle}>
+                {done && <MdDone />}
+            </div>
             <div className={`text ${done && 'done'}`}>{text}</div>
-            <div className={`remove ${removeActive && 'show'}`}>
+            <div className={`remove ${removeActive && 'show'}`} onClick={onRemove}>
                 <MdDelete />
             </div>
         </div>
     );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
