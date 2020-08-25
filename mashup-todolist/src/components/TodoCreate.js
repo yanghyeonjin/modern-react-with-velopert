cc14800763de2596a9b0519230acda12f37520ff
yanghyeonjin <guswls7081@gmail.com>
2020-08-25 10:30:45 +0900

    * TodoCreate 구현

--- mashup-todolist/src/components/TodoCreate.js
+++ mashup-todolist/src/components/TodoCreate.js
@@ -1,23 +1,52 @@
 // module
 import React, { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 // css
 import './TodoCreate.css';
 
 function TodoCreate() {
     const [open, setOpen] = useState(false); // 투 두 생성 폼의 open 여부
+    const [value, setValue] = useState(''); // input 관리
 
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+
+    // open/close 토클
     const onToggle = () => {
         setOpen(!open);
+    };
+
+    // input change
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    // 새 할 일 저장
+    const onSubmit = (e) => {
+        e.preventDefault(); // 새로고침 방지
+
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false,
+            },
+        });
+
+        setValue(''); // input 값 초기화
+        setOpen(false); // 다시 닫기
+        nextId.current += 1; // 다음 todo id 증가
     };
 
     return (
         <>
             {open && (
                 <div className="todo-create">
-                    <form>
-                        <input type="text" autoFocus placeholder="할 일 입력 후, Enter를 누르세요." />
+                    <form onSubmit={onSubmit}>
+                        <input type="text" autoFocus placeholder="할 일 입력 후, Enter를 누르세요." onChange={onChange} value={value} />
                     </form>
                 </div>
             )}
@@ -28,4 +57,4 @@ function TodoCreate() {
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
