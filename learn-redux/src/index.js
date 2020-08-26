c965bd0b787553cd7f47bbf6c510ea0a314295ca
yanghyeonjin <guswls7081@gmail.com>
2020-08-26 10:31:23 +0900

    * 리덕스 개발자도구 적용하기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -7,8 +7,9 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules/index';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 
 ReactDOM.render(
     <React.StrictMode>
